35a2999c5bdca7df88a7df99a63e452c55ecb41b
NE3ULA <affinitymojo2@gmail.com>
2026-03-10 21:00:01 -0700

    Add centralized asset registry

---
+++ sveltekit-app/src/lib/assets/registry.ts
@@ -0,0 +1,16 @@
+export const ASSETS = {
+  nebula: {
+    mark: '/assets/shared/nebula-mark.png'
+  },
+
+  backgrounds: {
+    enter: '/assets/shared/bg-enter-v2.png',
+    home: '/assets/home/bg-home.png',
+    homeMobile: '/assets/home/bg-home-mobile.png'
+  },
+
+  ui: {
+    beginButton: '/assets/enter/btn-begin.png',
+    trinityLine: '/assets/enter/trinity-line.png'
+  }
+};
