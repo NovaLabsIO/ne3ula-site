184fb203d82f37e9774e0fd0604f0fb6ff8af9dd
NE3ULA <affinitymojo2@gmail.com>
2026-03-10 21:14:13 -0700

    Add NE3ULA system kernel

---
+++ sveltekit-app/src/lib/ne3ula/system.ts
@@ -0,0 +1,32 @@
+/*
+NE3ULA SYSTEM KERNEL
+Central registry for world architecture.
+This will eventually power AXZIO and the NE3ULAVERSE engine.
+*/
+
+export const NE3ULA = {
+  world: {
+    name: "NE3ULAVERSE",
+    version: "0.1",
+  },
+
+  pillars: [
+    "Body",
+    "Mind",
+    "Heart",
+    "Spirit"
+  ],
+
+  layers: [
+    "Myth",
+    "Identity",
+    "Engine",
+    "Interface"
+  ],
+
+  interfaces: {
+    web: true,
+    axzio: true,
+    mobile: false
+  }
+}
